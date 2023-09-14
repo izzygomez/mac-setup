b48a8c669038e8aea190809fc534e0832dfdb419
Izzy Gomez <izzygmit@gmail.com>
2023-09-14 01:33:11 -0400

    Split `brew-installs.sh` & `casks.sh` definitions

--- brew-installs.sh
+++ brew-installs.sh
@@ -33,43 +33,7 @@
 #
 ################################################################################
 
-# For a full list, see https://formulae.brew.sh/cask/
-casks_to_install=(
-    'alfred'
-    'anki'
-    'atom'
-    'authy'
-    'beardedspice' # https://beardedspice.github.io/
-    'contexts'
-    'discord'
-    'dropbox'
-    'expressvpn'
-    'firefox'
-    'flux'
-    'freedom'
-    'google-chrome'
-    'google-drive'
-    'iterm2'
-    'kyokan-bob'
-    'lastpass'
-    'little-snitch'
-    'musescore'
-    'raspberry-pi-imager'
-    'rectangle'
-    'rescuetime'
-    'rocket'
-    'sonos'
-    'spotify'
-    'steam'
-    'telegram'
-    'tor-browser'
-    'transmission'
-    'trezor-bridge'
-    'trezor-suite'
-    'visual-studio-code'
-    'vlc'
-    'vnc-viewer'
-)
+source ./casks.sh
 
 packages_to_install=(
     'chruby' # from https://jekyllrb.com/docs/installation/macos/
