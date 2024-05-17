bd934b461979c686b6c346bc7161abecd218a8f6
Izzy Gomez <izzygmit@gmail.com>
2024-05-17 14:49:08 -0400

    add `openjdk` (aka `java`) to packages

--- packages.sh
+++ packages.sh
@@ -16,6 +16,11 @@ packages_to_install=(
     'mosh'
     'neofetch'  # finally found out the the thing all of r/unixporn uses
     'node'
+    # Java.
+    # Per [1], note that following command should be run after installation:
+    # sudo ln -sfn $HOMEBREW_PREFIX/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+    # [1] https://formulae.brew.sh/formula/openjdk
+    'openjdk'
     # See https://github.com/nvm-sh/nvm for why Homebrew installation is not
     # recommended. Keeping this line here for posterity & to make it clear that
     # nvm should be installed via script in GitHub repo.
